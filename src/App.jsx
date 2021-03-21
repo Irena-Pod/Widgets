be3416baba233790f77cd4ca508ec21b6645d5f7
Irena-Pod <irena.podgurski@gmail.com>
2021-03-20 20:22:39 -0400

    Add accordion component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import Accordion from './components/Accordion';
 
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front end JavaScript framework.',
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a favorite JS library amoing engineers.',
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating components. ',
+  },
+];
+
 const App = () => {
   return (
     <div>
-      <Accordion />
+      <Accordion items={items} />
     </div>
-  )
+  );
 };
 
 export default App;
