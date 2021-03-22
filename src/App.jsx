b99c6451cbceb0a59a20e4b512cdd79903986dd2
Irena-Pod <irena.podgurski@gmail.com>
2021-03-22 11:51:52 -0400

    Add dropdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
   {
@@ -16,11 +17,27 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: 'The color Red',
+    value: 'red',
+  },
+  {
+    label: 'The color Green',
+    value: 'green',
+  },
+  {
+    label: 'A shade of Blue',
+    value: 'blue',
+  },
+];
+
 const App = () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
-      <Search />
+      {/* <Search /> */}
+      <Dropdown options={options} />
     </div>
   );
 };
