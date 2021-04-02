fbdea00dc53ff4b465537433608e15701abc519c
Irena-Pod <irena.podgurski@gmail.com>
2021-04-02 11:24:18 -0400

    Add Route component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import { useState } from 'react';
 import Translate from './components/Translate';
+import Route from './components/Route';
+
 const items = [
   {
     title: 'What is React?',
@@ -34,18 +36,27 @@ const options = [
 ];
 
 const App = () => {
-  // const [selected, setSelected] = useState(options[0]);
+  const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <Dropdown
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
           selected={selected}
           onSelectedChange={setSelected}
-          options={options}
-        /> */}
+        />
+      </Route>
+      <Route path="/translate">
         <Translate />
+      </Route>
     </div>
   );
 };
